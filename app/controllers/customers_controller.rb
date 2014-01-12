1e757c29aba186bc4c6040e0b319fcf4cf1668dd
Matt Burke <spraints@gmail.com>
2014-01-12 15:10:18 -0500

    List customers.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,12 @@
 class CustomersController < ApplicationController
+  def index
+    vm.customers = Customer.all
+  end
+
+  private
+
+  helper_method :vm
+  def vm
+    @vm ||= OpenStruct.new
+  end
 end
