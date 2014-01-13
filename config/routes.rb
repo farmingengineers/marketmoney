e8c5344938034db6e0e905dc6e9e4b36ba52ff99
Matt Burke <spraints@gmail.com>
2014-01-13 06:41:10 -0500

    Create routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :customers
+
+  root :to => "customers#index"
+  get "market" => "customers#edit_all"
+  patch "market" => "customers#update_all"
+
+  get ":slug" => "customers#show_self"
 end
---
+++ spec/routing/all_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'routing' do
+  context 'farmer' do
+    it { expect(get('/')).to route_to("customers#index") }
+    it { expect(get('/customers/new')).to route_to("customers#new") }
+    it { expect(post('/customers')).to route_to("customers#create") }
+    it { expect(get('/customers/1')).to route_to("customers#show", :id => '1') }
+    it { expect(get('/customers/1/edit')).to route_to("customers#edit", :id => '1') }
+    it { expect(patch('/customers/1')).to route_to("customers#update", :id => '1') }
+    it { expect(get('/market')).to route_to("customers#edit_all") }
+    it { expect(patch('/market')).to route_to("customers#update_all") }
+  end
+
+  context 'member' do
+    it { expect(get('/5a5e56f')).to route_to("customers#show_self", :slug => '5a5e56f') }
+  end
+end
