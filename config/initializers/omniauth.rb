b454b3d42eadc25ecd3ddbbe2344e25ad821a9b9
Matt Burke <spraints@gmail.com>
2014-02-07 07:47:05 -0500

    Auth with the google.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :openid, OpenID::Store::Filesystem.new('./tmp')
+  provider :openid,
+    :store => OpenID::Store::Filesystem.new('./tmp'),
+    :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 end
