249e7a1e932dba32992b80825b72a4b0f3289913
Matt Burke <spraints@gmail.com>
2014-02-09 20:17:00 -0500

    Slugs for customers.

--- Rakefile
+++ Rakefile
@@ -4,3 +4,10 @@
 require File.expand_path('../config/application', __FILE__)
 
 Rails.application.load_tasks
+
+desc "Ensure all customers have slugs"
+task :slugs => :environment do
+  Customer.all.each do |customer|
+    customer.slug_me!
+  end
+end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,19 @@ class Customer < ActiveRecord::Base
   def balance
     transactions.sum(:amount)
   end
+
+  # Public: Ensure this customer has a slug.
+  def slug_me!
+    if slug.nil?
+      update_attribute :slug, build_slug
+    end
+  end
+
+  private
+
+  def build_slug
+    digest = Digest::MD5.digest("#{id}:#{Time.now.to_i}:#{Rails.application.secrets.secret_key_base}")
+    b64digest = Base64.urlsafe_encode64(digest)
+    b64digest[0,8]
+  end
 end
