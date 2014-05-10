20471e67ba910a52d948444112f475d418899893
Matt Burke <spraints@gmail.com>
2014-05-10 16:43:44 -0400

    Make a stylesheet for printing.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module MarketMoney
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.precompile << "print.css"
   end
 end
