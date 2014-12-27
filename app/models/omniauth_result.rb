4cd75b7a5b71e0b973809d4fb732c50d8af337a6
Matt Burke <spraints@gmail.com>
2014-12-27 10:07:56 -0500

    Extract a method.

--- app/models/omniauth_result.rb
+++ app/models/omniauth_result.rb
@@ -5,6 +5,14 @@ class OmniauthResult
 
   # Public: The user associated with the omniauth auth hash.
   def user
-    @user ||= Admin.where(:provider => @auth.provider, :uid => @auth.uid).first_or_create.tap { |u| u.update_attribute :username, @auth.info.email }
+    @user ||= find_or_create_user
+  end
+
+  private
+
+  def find_or_create_user
+    user = Admin.where(:provider => @auth.provider, :uid => @auth.uid).first_or_create
+    user.update_attribute :username, @auth.info.email
+    user
   end
 end
