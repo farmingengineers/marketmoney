3d05e6fc36080d2ae7c7d00ba030b7b4280f534d
Matt Burke <spraints@gmail.com>
2020-07-07 15:09:28 -0400

    Update rails to latest 5.1

--- db/migrate/20140112201759_add_notes_to_customers.rb
+++ db/migrate/20140112201759_add_notes_to_customers.rb
@@ -1,4 +1,4 @@
-class AddNotesToCustomers < ActiveRecord::Migration
+class AddNotesToCustomers < ActiveRecord::Migration[4.2]
   def change
     add_column :customers, :notes, :text
   end
--- db/migrate/20141227151324_unique_username.rb
+++ db/migrate/20141227151324_unique_username.rb
@@ -1,4 +1,4 @@
-class UniqueUsername < ActiveRecord::Migration
+class UniqueUsername < ActiveRecord::Migration[4.2]
   def change
     add_index :admins, [:provider, :username], :unique => true
   end
