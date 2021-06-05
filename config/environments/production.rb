f54739cb80660e22072ac498cd2290e61c701ad8
Matt Burke <spraints@gmail.com>
2021-06-05 09:16:18 -0400

    Make everything use https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = {hsts: {expires: 1.day}}
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
